8d27a67a9fbb94030dba5629af7478468d19c368
twinklekailashlahariya <twinklelahariya1@gmail.com>
2022-03-29 11:11:47 +0530

    Add roll and score and compute score 0 when all rolls are zero

--- src/main/kotlin/Game.kt
+++ src/main/kotlin/Game.kt
@@ -1,3 +1,13 @@
 class Game {
 
+    private val scores = ArrayList<Int>()
+    fun roll( rolls : Int){
+        scores.add(rolls)
+    }
+
+    fun score(): Int{
+
+        return scores.reduce{ a , b -> a + b }
+    }
+
 }
--- src/test/kotlin/GameTest.kt
+++ src/test/kotlin/GameTest.kt
@@ -1,5 +1,15 @@
+import junit.framework.Assert.assertEquals
 import org.junit.Test
 
 class GameTest {
 
+    private val game = Game()
+
+    @Test
+    fun shouldReturnScore0_WhenAllRolls0() {
+        for(x in 1..20){
+            game.roll(0)
+        }
+        assertEquals(0, game.score())
+    }
 }

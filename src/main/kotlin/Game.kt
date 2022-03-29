f79b2aca18b369646f91614c262a6f759ac54c76
twinklekailashlahariya <twinklelahariya1@gmail.com>
2022-03-29 16:23:57 +0530

    Add Frames with not more than 10 pins

--- src/main/kotlin/Game.kt
+++ src/main/kotlin/Game.kt
@@ -1,13 +1,26 @@
 class Game {
 
     private val scores = ArrayList<Int>()
+
+    private var remainingPins = 0
+
     fun roll( rolls : Int){
-        scores.add(rolls)
+        if(scores.size % 2 == 0){
+            remainingPins = 10 - rolls
+            if(remainingPins > 0) {
+                scores.add(rolls)
+            }
+        }
+        else {
+            if(remainingPins >= rolls) {
+                scores.add(rolls)
+            }
+            remainingPins = 0
+        }
     }
 
     fun score(): Int{
 
         return scores.reduce{ a , b -> a + b }
     }
-
 }
--- src/test/kotlin/GameTest.kt
+++ src/test/kotlin/GameTest.kt
@@ -7,9 +7,40 @@ class GameTest {
 
     @Test
     fun shouldReturnScore0_WhenAllRolls0() {
-        for(x in 1..20){
+        rollZeroes(20)
+
+        assertEquals(0, game.score())
+    }
+
+    @Test
+    fun shouldReturnScore1_WhenOneRollIs1() {
+        rollZeroes(19)
+        game.roll(1)
+
+        assertEquals(1, game.score())
+    }
+
+    private fun rollZeroes(n : Int) {
+        for (x in 1..n) {
             game.roll(0)
         }
-        assertEquals(0, game.score())
+    }
+
+    @Test
+    fun shouldBeAbleToRollMax10PinsInOneFrame(){
+        game.roll(9)
+        game.roll(1)
+        rollZeroes(18)
+
+        assertEquals(10, game.score())
+    }
+
+    @Test
+    fun shouldBeAbleToRollMax10PinsInOneFrame2(){
+        game.roll(9)
+        game.roll(12)
+        rollZeroes(18)
+
+        assertEquals(9, game.score())
     }
 }
